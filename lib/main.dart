c2456da8f8aa6f4192675f1452685c18f8581e94
Orhan Demir <121957414+Orhandem4@users.noreply.github.com>
2025-07-01 21:20:34 +0300

    1 day end

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:e_commerce_app/pages/favorite_screen.dart';
+import 'package:e_commerce_app/pages/home_screen.dart';
+import 'package:e_commerce_app/pages/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,10 +14,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Text("selam"),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int currentIndex = 0;
+  List screens = [
+  const HomeScreen(),
+  const FavoriteScreen(),
+  const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("E - Commerce Shop"),
+        centerTitle: true,
+      ),
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() => currentIndex = value);
+        },
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: "Favorite",
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,115 @@
+
+
+import 'package:e_commerce_app/models/my_product.dart';
+import 'package:e_commerce_app/widgets/product_card.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int isSelected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          const Text(
+            "Our Products",
+            style: TextStyle(
+              fontSize: 27,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildProductCategory(index: 0, name: "All Products"),
+              _buildProductCategory(index: 1, name: "Jackets"),
+              _buildProductCategory(index: 2, name: "Sneakers"),
+            ],
+          ),
+          const SizedBox(height: 20,),
+          Expanded(
+            child: isSelected == 0 
+            ? _buildAllProducts() 
+            : isSelected == 1
+            ? _buildJackets()
+            : _buildSneakers(), 
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildProductCategory({required int index, required String name}) => 
+  GestureDetector(
+    onTap: () => setState(() => isSelected = index),
+    child: Container(
+      width: 100,
+      height: 40,
+      margin: const EdgeInsets.only(top: 10, right: 10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isSelected == index ? Colors.red : Colors.red.shade300,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        name,
+        style: const TextStyle(color: Colors.white),
+      ),
+    ),
+  );
+
+  _buildAllProducts() => GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: (100 / 140),
+      crossAxisSpacing: 12,
+      mainAxisSpacing: 12,
+    ),
+    scrollDirection: Axis.vertical,
+    itemCount: MyProducts.allProducts.length,
+    itemBuilder: (context,index) {
+      final allProducts = MyProducts.allProducts[index];
+      return ProductCard(product: allProducts);
+    },
+  );
+
+  _buildJackets() => GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: (100 / 140),
+      crossAxisSpacing: 12,
+      mainAxisSpacing: 12,
+    ), 
+    scrollDirection: Axis.vertical,
+    itemCount: MyProducts.jacketsList.length,
+    itemBuilder: (context,index) {
+      final jacketsList = MyProducts.jacketsList[index];
+      return ProductCard(product: jacketsList);
+    },
+  );
+
+  _buildSneakers() => GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: (100 / 140),
+      crossAxisSpacing: 12,
+      mainAxisSpacing: 12,
+    ), 
+    scrollDirection: Axis.vertical,
+    itemCount: MyProducts.sneakersList.length,
+    itemBuilder: (context, index) {
+      final sneakersList = MyProducts.sneakersList[index];
+      return ProductCard(product: sneakersList);
+    },
+  );
+}
