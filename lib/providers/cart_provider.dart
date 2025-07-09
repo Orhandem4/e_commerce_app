743390f317444b98f490940cd210dd32b4668d70
Orhan Demir <orhandem4@gmail.com>
2025-07-09 13:59:50 +0300

    day 6 end

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -2,7 +2,7 @@ import 'package:e_commerce_app/models/product.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CartProvider extends ChangeNotifier{
+class CartProvider extends ChangeNotifier {
   final List<Product> _cart = [];
   List<Product> get cart => _cart;
 
@@ -11,15 +11,25 @@ class CartProvider extends ChangeNotifier{
       for (Product element in _cart) {
         element.quantity++;
       }
-    }else{
+    } else {
       _cart.add(product);
     }
 
     notifyListeners();
   }
 
+  incrementQuantity(int index) => _cart[index].quantity++;
+  decrementQuantity(int index) => _cart[index].quantity--;
+
+  getTotalPrice() {
+    double total = 0.0;
+    for (Product element in _cart) {
+      total += element.price * element.quantity;
+    }
+    return total;
+  }
+
   static CartProvider of(BuildContext context, {bool listen = true}) {
     return Provider.of<CartProvider>(context, listen: listen);
   }
-
 }
